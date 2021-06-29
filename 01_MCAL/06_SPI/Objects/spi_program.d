fbc65a1ae1eb600b3e7839a8530a1564500719cd
abdelrahman <abdelrahmanmagdy508@gmail.com>
2021-06-29 03:32:18 +0200

    SPI_Without_Main

---
+++ 01_MCAL/06_SPI/Objects/spi_program.d
@@ -0,0 +1,2 @@
+./objects/spi_program.o: SPI_program.c STD_TYPES.h BIT_MATH.h \
+  SPI_private.h SPI_interface.h SPI_config.h
---
+++ 01_MCAL/06_SPI/main.c
@@ -0,0 +1,26 @@
+
+int main(void)
+{
+	/* Config RCC GPIO SPI */
+	
+	/*Init GPIO*/
+	
+	/* Nvic Init En SPI From Vector Table SPI1-->35*/
+	
+	/* SPI Init */
+	
+	/* Active Intterput OF  TXEIE & RXNEIE CR2*/
+	
+	/* GPIO At HW MODE MOSI(AF,OUTPUT)Like SPI1--->AF(5) */
+	
+	/* GPIO At HW MODE NSS(AF,OUTPUT)Like SPI1--->AF(5) */
+	
+	/* GPIO At HW MODE MISO(AF,INPUT)Like SPI1--->AF(5) */
+	
+	
+	
+}
+
+/* When Flag Come Call ISR And Know Which Raise */
+/*Handelr Read  */
+/*Handelr Write */
